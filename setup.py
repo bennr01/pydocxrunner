34143ec75c56145aa742a4c88b0bde9c6a1f1605
bennr01 <benjamin99.vogt@web.de>
2020-04-09 23:27:36 +0200

    setup.py: fix README content type

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ from setuptools import setup
 
 setup(
     name="pydocxrunner",
-    version="0.0.1",
+    version="0.0.2",
     description="Make .docx files executable for the python interpreter",
     long_description=open('README.md', "r").read(),
+    long_description_content_type="text/markdown",
     keywords="docx executable",
     author="bennr01",
     url="https://github.com/bennr01/pydocxrunner",
